6c09ce21cb0280050db889b5ec7c8416034e8105
Bill MK <bilgatematiba8300@gmail.com>
2026-02-24 12:41:36 +0300

    I have imported the louncher module, in this script , meaning the bot can't work withwithout this module

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 from playwright.sync_api import sync_playwright
+from launcher import browser_trigger as bt
+import time
 
+def site_crawling(query):
+    p, browser, context, page = bt("https://www.google.com")
+    
+    # Search for the sites
+    page.click("input[names=q]")
+    page.keyboard.type(query, delay=100)
+    page.keyboard.press('Enter')  
+    browser.close()
+    p.stop()
+    
+
+site_crawling()
